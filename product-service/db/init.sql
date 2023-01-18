ca6c11206dce58e10607ee8dc603e15caf12bb32
Kevin Phillips <kevin@bitovi.com>
2023-01-17 23:17:26 -0600

    app setup

--- product-service/db/init.sql
+++ product-service/db/init.sql
@@ -1,23 +1,10 @@
-CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
+create extension if not exists "uuid-ossp";
 
-create database tax;
+create database product;
 
-\c tax
+\c product
 
-create table tax_rules
+create table product
 (
-  id          UUID primary key,
-  name        text not null,
-  percentage  decimal not null,
-  deleted     boolean not null default false
-);
-
-create table event_log
-(
-  id          SERIAL PRIMARY KEY,
-  entity      text not null,
-  entity_id   UUID not null,
-  version     integer not null,
-  data        jsonb not null,
-  unique(entity, entity_id, version)
+  id          UUID primary key
 );
