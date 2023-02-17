3cb4ddd4118a218fc42b11a1526e90e59ff8c9c7
Nicole Greene <nicolegilbert16@gmail.com>
2023-02-17 09:52:40 -0800

    update

--- db/seeders/2-loan_applications.js
+++ db/seeders/2-loan_applications.js
@@ -2,29 +2,29 @@
 module.exports = {
   async up(queryInterface) {
     await queryInterface.bulkInsert('loan_applications', [
-      {
+      { // doesn't qualify
         uuid: '19b75a10-cb68-4df9-83c3-16f752aba227',
         current_balance: null,
         requested_amount: 11000,
         applicant: '3ea2c3c2-ca8f-46bd-bb91-1941bc7fc048',
         status: 'pending',
-        interest_rate_uuid: null
+        interest_rate_uuid: '97d9edc3-34bf-4832-bc3d-76b9c867472c'
       },
-      {
+      { // invalid interest rate
         uuid: '8f6231c5-24fb-4793-92ba-81d5cfce774d',
         current_balance: null,
         requested_amount: 250000,
         applicant: '7643924d-a556-4bf5-ad00-13130f0c8acf',
         status: 'pending',
-        interest_rate_uuid: null
+        interest_rate_uuid: 'invalid'
       },
-      {
+      { // valid application
         uuid: '0685141e-f1b4-4c8b-81ed-c71d585a6081',
         current_balance: null,
         requested_amount: 75000,
         applicant: 'a6dc157d-575b-410d-9be7-f20248772095',
         status: 'pending',
-        interest_rate_uuid: null
+        interest_rate_uuid: '97d9edc3-34bf-4832-bc3d-76b9c867472c'
       }
     ])
   }
--- src/hooks.ts
+++ src/hooks.ts
@@ -1,8 +1,26 @@
-import { LoanApplicationsModel } from './models'
+import { LoanApplicationsModel, RatesModel, UsersModel } from './models'
+import CreateError from 'http-errors'
 
-LoanApplicationsModel.addHook('beforeUpdate', (record) => {
-  // Set the current_balance when the loan is approved
-  if (record.previous('status') === 'pending' && record.getDataValue('status') === 'approved') {
-    record.setDataValue('current_balance', record.getDataValue('requested_amount'))
+// see an error message when attempting to approve a loan application with a user that has a credit_score that is less than the minimum_credit_score attached to the interest_rate_uuid provided, 
+// where the credit_score of the user is greater than the minimum_credit_score attached to the interest_rate_uuid provided and see that new status is approved on the loan
+// If a user has the same credit_score as the minimum_credit_score the loan should still be approved
+LoanApplicationsModel.addHook('beforeUpdate', async(record: any) => {
+  // Approve loan if credit score of user is greater than the
+  // minimum_credit_score attached to the interest_rate_uuid provided
+  const applicantId = record.getDataValue('applicant')
+  const interestRateId = record.getDataValue('interest_rate_uuid')
+
+  const interestRate = await RatesModel.findByPk(interestRateId)
+
+  if (!interestRate) {
+    throw CreateError(409, 'Invalid interest rate')
   }
+
+  const applicantRecord = await UsersModel.findByPk(applicantId)
+  console.log(applicantRecord)
+
+  // Set the current_balance when the loan is approved
+  // if (record.previous('status') === 'pending' && record.getDataValue('status') === 'approved') {
+  //   record.setDataValue('current_balance', record.getDataValue('requested_amount'))
+  // }
 })
